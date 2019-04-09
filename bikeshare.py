9613ff5ae3a81d8633221224b3de332204d10597
Michelle-Dorgan <michelle.m.dorgan@gmail.com>
2019-04-09 20:37:47 +0100

    minor readability update line 22

--- bikeshare.py
+++ bikeshare.py
@@ -19,9 +19,10 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input('\nChoose a city between Chicago, New York City, and Washington:\n> ').lower()  
+        city = input('\nChoose a city between Chicago, New York City, and Washington:\n> ').lower()
         if city not in cities:
             print('\n"{}" is not found in our records.'.format(city))
             continue
@@ -46,7 +47,7 @@ def get_filters():
         else:
             break
 
-   
+
     return city, month, day
 
 
@@ -65,26 +66,26 @@ def load_data(city, month, day):
 
     # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
-    
-    # convert the Start Time column to datetime     
-    df['Start Time'] = pd.to_datetime(df['Start Time']) 
-   
+
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
-    # filter by month if applicable    
+
+    # filter by month if applicable
     if month != 'all':
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
-        # filter by month to create the new dataframe        
+        # filter by month to create the new dataframe
         df = df[df['month'] == month]
-    
+
     # filter by day of week if applicable
-    if day != 'all': 
-        df = df[df['day_of_week'] == day.title()] 
-        
-     
+    if day != 'all':
+        df = df[df['day_of_week'] == day.title()]
+
+
     return df
 
 def time_stats(df):
@@ -101,7 +102,7 @@ def time_stats(df):
     # find the most popular month
     popular_month = df['month'].mode()[0]
     print('Most Popular Month: {}'.format(popular_month))
- 
+
 
     # TO DO: display the most common day of week
     # convert the Start Time column to datetime
@@ -140,11 +141,11 @@ def station_stats(df):
     common_end_station = df['End Station'].mode()[0]
     print('Most Popular End Station: {}'.format(common_end_station))
     print('End Station Counts: {}\n'.format(df['End Station'].value_counts()[common_end_station]))
-    
+
     # TO DO: display most frequent combination of start station and end station trip
     most_common_start_end_station = df[['Start Station', 'End Station']].mode().loc[0]
     print("The most commonly used start station and end station : {}, {}".format(most_common_start_end_station[0], most_common_start_end_station[1]))
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -172,29 +173,29 @@ def user_stats(df):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    
+
      # display counts of user types
     user_types = df['User Type'].value_counts()
     print('User Types: \n{}\n'.format(user_types))
-    
+
     try:
          # To manage errors from missing data in Washington file
          # TO DO: Display counts of gender
-         gender_types = df['Gender'].value_counts() 
-  
+         gender_types = df['Gender'].value_counts()
+
          # TO DO: Display earliest, most recent, and most common year of birth
          earliest_birth_year = df['Birth Year'].min()
-    
+
          most_recent_birth_year = df['Birth Year'].max()
-    
+
          common_birth_year = df['Birth Year'].mode()[0]
-    
+
     except KeyError:
          # print error statement for missing data
          print('Gender data not available. \nCannot display statistics.\n')
-    
+
          print('Birth Year data not available.  \nCannot display statistics.')
-    
+
     else:
          # display counts of gender
          print(gender_types)
@@ -204,7 +205,7 @@ def user_stats(df):
          print('Most Recent Birth Year:', most_recent_birth_year.min())
          # display most popular birth
          print('Most Popular Birth Year:', common_birth_year)
-    
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -212,8 +213,8 @@ def user_stats(df):
 
 def raw_data(df):
     """ Accesses csv and displays 5 rows of its raw data if user types "yes" """
- 
- 
+
+
     raw_data_request = input('\nWould you like to see 5 rows of raw data?  (Yes or No)\n> ').lower()
     if raw_data_request == 'yes':
         print('\nAccessing Raw Data...\n')
@@ -229,10 +230,10 @@ def raw_data(df):
             # breaks out of loop if user doesn't type "yes"
             if more_data_request != 'yes':
                 break
- 
- 
-    print('-'*40)      
-    
+
+
+    print('-'*40)
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -243,7 +244,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         raw_data(df)
-        
+
         """ Asks user to restart program and clears screen if yes """
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
@@ -253,7 +254,7 @@ def main():
             # clears screen: OSX & Linux
             if os_type in ('Darwin', 'Linux'):
                 os.system('clear')
-            # clears screen: Windows    
+            # clears screen: Windows
             elif os_type == 'Windows':
                 os.system('cls')
             # continues without clearing screen if unknown OS is detected
